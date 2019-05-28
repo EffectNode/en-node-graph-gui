617a95a19d4f98f78bfd5116f76fb141646e67a8
Wong Lok <yellowhappy831@gmail.com>
2019-05-28 20:24:23 +0800

    front-end-deploy

--- src/components/Explaination.vue
+++ src/components/Explaination.vue
@@ -253,4 +253,9 @@ export default {
 
 <style scoped>
 @import url(./columns.css);
+.oo-div:hover .oo-content-title{
+  background-image: linear-gradient(161deg, #c0392b 0%, #f39c12 20%,
+  #f1c40f 40%,#2ecc71 60%, #3498db 80%, #8e44ad 100%);
+  border-radius: 5px 5px 0px 0px;
+}
 </style>
