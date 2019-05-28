a1dd57608f9161414259689f5bd3f5fb8ada4ec7
Wong Lok <yellowhappy831@gmail.com>
2019-05-28 15:30:41 +0800

    front-end-deploy

--- src/components/Explaination.vue
+++ src/components/Explaination.vue
@@ -1,63 +1,68 @@
 <template>
   <div class="oo-container">
+    <div class="oo-section-title">
+      <h1>
+        Features
+      </h1>
+    </div>
     <div class="oo-row">
       <div class="oo-div">
         <div class="oo-wrap">
           <div class="oo-content">
-            <p class="oo-content-title">
+            <div class="oo-content-title">
               Mix and Match
-            </p>
-            <p>
+            </div>
+            <div class="oo-content-body">
               You can use drag abd drop to mix and match: Materail, Geometry, Mesh, Pount, Lines
-            </p>
+            </div>
           </div>
         </div>
       </div>
       <div class="oo-div">
         <div class="oo-wrap">
           <div class="oo-content">
-            <p class="oo-content-title">
+            <div class="oo-content-title">
               Cinematic
-            </p>
-            <p>
+            </div>
+            <div class="oo-content-body">
               You can leverage our Timeline with Multiple Scenes and Cameras to produce a small computer graphcis based movie.
-            </p>
+            </div>
           </div>
         </div>
       </div>
       <div class="oo-div">
         <div class="oo-wrap">
           <div class="oo-content">
-            <p class="oo-content-title">
+            <div class="oo-content-title">
               Recycle Bin
-            </p>
-            <p>
+            </div>
+            <div class="oo-content-body">
               You can use move items to recycle bin or restore them or permenmently delete them.
-            </p>
+            </div>
           </div>
         </div>
       </div>
       <div class="oo-div">
         <div class="oo-wrap">
           <div class="oo-content">
-            <p class="oo-content-title">
+            <div class="oo-content-title">
               Support Custom Code.
-            </p>
-            <p>
+            </div>
+            <div class="oo-content-body">
               You can edit the code of each ball item, which means you nearly have all the customistaion of details to the level of custom coding.
-            </p>
+            </div>
           </div>
         </div>
       </div>
       <div class="oo-div">
         <div class="oo-wrap">
           <div class="oo-content">
-            <p class="oo-content-title">
+            <div class="oo-content-title">
               Visualise your content structure.
-            </p>
-            <p>
-              Our content tree lets you organise content in a visual way. App has many scene, each scene is about it's own contnet.
-            </p>
+            </div>
+            <div class="oo-content-body">
+              Our content tree lets you organise content in a visual way, so that you can easily navigate through your content and edit them.
+            </div>
           </div>
         </div>
       </div>
