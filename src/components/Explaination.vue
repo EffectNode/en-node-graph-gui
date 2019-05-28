e1f2356e9605cf44d517860e9a98d45d1056aced
Wong Lok <yellowhappy831@gmail.com>
2019-05-28 15:03:53 +0800

    front-end-deploy

--- src/components/Explaination.vue
+++ src/components/Explaination.vue
@@ -1,6 +1,66 @@
 <template>
-  <div class="explain">
-    <div class="ex-row">
+  <div class="oo-container">
+    <div class="oo-row">
+      <div class="oo-div">
+        <div class="oo-wrap">
+          <div class="oo-content">
+            <p class="oo-content-title">
+              Mix and Match
+            </p>
+            <p>
+              You can use drag abd drop to mix and match: Materail, Geometry, Mesh, Pount, Lines
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="oo-div">
+        <div class="oo-wrap">
+          <div class="oo-content">
+            <p class="oo-content-title">
+              Cinematic
+            </p>
+            <p>
+              You can leverage our Timeline with Multiple Scenes and Cameras to produce a small computer graphcis based movie.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="oo-div">
+        <div class="oo-wrap">
+          <div class="oo-content">
+            <p class="oo-content-title">
+              Recycle Bin
+            </p>
+            <p>
+              You can use move items to recycle bin or restore them or permenmently delete them.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="oo-div">
+        <div class="oo-wrap">
+          <div class="oo-content">
+            <p class="oo-content-title">
+              Support Custom Code.
+            </p>
+            <p>
+              You can edit the code of each ball item, which means you nearly have all the customistaion of details to the level of custom coding.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="oo-div">
+        <div class="oo-wrap">
+          <div class="oo-content">
+            <p class="oo-content-title">
+              Visualise your content structure.
+            </p>
+            <p>
+              Our content tree lets you organise content in a visual way. App has many scene, each scene is about it's own contnet.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -12,11 +72,5 @@ export default {
 </script>
 
 <style scoped>
-.explain{
-  color: white;
-  background-color: black;
-}
-.ex-row {
-
-}
+@import url(./columns.css);
 </style>
---
+++ src/components/Template.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="oo-container">
+    <div class="oo-row">
+      <div class="oo4">
+        123
+      </div>
+      <div class="oo4">
+        123
+      </div>
+      <div class="oo4">
+        123
+      </div>
+      <div class="oo4">
+        123
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+@import url(./columns.css);
+</style>
